2c84f61279d2369c39693ab2d3d26c896fc1d328
Shr1ram741 <shriram9625@gmail.com>
2025-06-17 10:17:46 +0530

    Create shell-b.c

---
+++ assignment2/shell/shell-b.c
@@ -0,0 +1,103 @@
+#include  <stdio.h>
+#include  <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_TOKEN_SIZE 64
+#define MAX_NUM_TOKENS 64
+
+/* Splits the string by space and returns the array of tokens
+*
+*/
+char **tokenize(char *line)
+{
+  char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
+  char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+  int i, tokenIndex = 0, tokenNo = 0;
+
+  for(i =0; i < strlen(line); i++){
+
+    char readChar = line[i];
+
+    if (readChar == ' ' || readChar == '\n' || readChar == '\t'){
+      token[tokenIndex] = '\0';
+      if (tokenIndex != 0){
+	tokens[tokenNo] = (char*)malloc(MAX_TOKEN_SIZE*sizeof(char));
+	strcpy(tokens[tokenNo++], token);
+	tokenIndex = 0; 
+      }
+    } else {
+      token[tokenIndex++] = readChar;
+    }
+  }
+ 
+  free(token);
+  tokens[tokenNo] = NULL ;
+  return tokens;
+}
+
+
+int main(int argc, char* argv[]) {
+	char  line[MAX_INPUT_SIZE];            
+	char  **tokens;              
+	int i;
+
+	while(1) {			
+		/* BEGIN: TAKING INPUT */
+		bzero(line, sizeof(line));
+    char cwd[256];
+    getcwd(cwd,sizeof(cwd));
+		printf("%s $ ",cwd);
+		scanf("%[^\n]", line);
+		getchar();
+
+		//printf("Command entered: %s (remove this debug output later)\n", line);
+		/* END: TAKING INPUT */
+    if (line[0]=='\0') {continue;}
+
+		line[strlen(line)] = '\n'; //terminate with new line
+		tokens = tokenize(line);
+   
+       //do whatever you want with the commands, here we just print them
+    if (strcomp(tokens[0],"cd")==0) {
+        if (chdir(tokens[1]) != 0) {
+          perror("chdir failed");
+        }
+    }
+
+    int status;
+    pid_t pid = fork();
+    if (pid<0) {
+      perror("Fork failed");
+    }
+    else if (pid==0) {
+      if (strcmp(tokens[0],"cd")!=0) {
+        execvp(tokens[0],tokens);
+
+        perror("Exec failed");
+        exit(1);
+      }
+    }
+    else {
+      pid_t cpid = wait(&status);
+
+      if (WIFEXITED(status)) {
+        int c_status = WEXITSTATUS(status);
+        printf("EXITSTATUS: %d",c_status);      
+    }
+
+		//for(i=0;tokens[i]!=NULL;i++){
+		//	printf("found token %s (remove this debug output later)\n", tokens[i]);
+		//}
+       
+		// Freeing the allocated memory	
+		for(i=0;tokens[i]!=NULL;i++){
+			free(tokens[i]);
+		}
+		free(tokens);
+
+	}
+	return 0;
+}
