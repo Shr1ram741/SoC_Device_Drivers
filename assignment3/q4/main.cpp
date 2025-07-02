e557ff086ebe64d17b51f391d9217778881a94fb
Shr1ram741 <shriram9625@gmail.com>
2025-07-03 01:20:36 +0530

    Create main.cpp

---
+++ assignment3/q4/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "MMU.hpp"
+#include "Memory.hpp"
+#include "Config.h"
+
+using namespace std
+
+int main() {
+    try {
+        Config config;
+        config.addressWidth = 32;
+        config.pageSize = 4096;
+        config.pageTableLevels = 2;
+        config.bitsPerLevel = {10,10};
+
+        Memory mem(config);
+        MMU mmu(mem, config);
+
+        uint64_t vaddr1 = 0x12345000;
+        uint64_t vaddr2 = 0x12345001;
+
+        mmu.storeVal(vaddr1, 23);
+        mmu.storeVal(vaddr2, 729);
+
+        cout << mmu.loadVal(vaddr1) << endl;
+        cout << mmu.loadVal(vaddr2) << endl;
+
+        mmu.loadVal(0x00000000);
+
+    } catch (runtime_error& e) {
+        cerr << "Exception: " << e.what() << endl;
+    }
+}
