470f930cc692bba290fda5e13b1290253b67c612
Shr1ram741 <shriram9625@gmail.com>
2025-07-03 00:14:08 +0530

    Update Memory.cpp

--- assignment3/q1/Memory.cpp
+++ assignment3/q1/Memory.cpp
@@ -24,14 +24,14 @@ class Memory {
 
     PageTableEntry getPageTableEntry(uint32_t level, uint32_t index) const {
       if (level >= pageTables.size()) {
-          throw std::runtime_error("Invalid Page Table Level");
+          throw runtime_error("Invalid Page Table Level");
       }
       auto PTE = pageTables[level].find(index);
       if (PTE == pageTables[level].end()) {
-          throw std::runtime_error("Invalid Virtual Address");
+          throw runtime_error("Invalid Virtual Address");
       }
       else if (!(PTE->second.present)) {
-          throw std::runtime_error("Page Fault");
+          throw runtime_error("Page Fault");
       }
       return PTE->second;
     }
