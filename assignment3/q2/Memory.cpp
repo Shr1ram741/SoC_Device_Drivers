9da9d41d5dbd5210f370265c284aa9cdd89e1c65
Shr1ram741 <shriram9625@gmail.com>
2025-07-03 00:17:02 +0530

    Create Memory.cpp

---
+++ assignment3/q2/Memory.cpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+#include <unordered_map>
+#include "Config.h"
+#include "Memory.hpp"
+#include "MMU.hpp"
+
+using namespace std
+
+class Memory {
+  friend class MMU;
+  public:
+    Memory(Config& cf): nextAvailableFrame(0) {
+      pageTables.resize(cf.pageTablesLevels);
+    }
+  private:
+    void setPageTableEntry(uint32_t level, uint32_t index, const PageTableEntry& entry) {
+      if (level >= pageTables.size()) {
+        throw runtime_error("Invalid Page Table Level");
+      }
+      pageTables[level][index]=entry;
+    }
+
+    PageTableEntry getPageTableEntry(uint32_t level, uint32_t index) const {
+      if (level >= pageTables.size()) {
+          throw runtime_error("Invalid Page Table Level");
+      }
+      auto PTE = pageTables[level].find(index);
+      if (PTE == pageTables[level].end()) {
+          throw runtime_error("Invalid Virtual Address");
+      }
+      else if (!(PTE->second.present)) {
+          throw runtime_error("Page Fault");
+      }
+      return PTE->second;
+    }
+};
