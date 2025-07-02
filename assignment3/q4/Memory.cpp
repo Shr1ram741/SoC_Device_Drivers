af4566cfa528d247f1cf5c8874193329ca73801e
Shr1ram741 <shriram9625@gmail.com>
2025-07-03 00:30:23 +0530

    Create Memory.cpp

---
+++ assignment3/q4/Memory.cpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+#include <unordered_map>
+#include "Config.h"
+#include "Memory.hpp"
+#include "MMU.hpp"
+
+using namespace std
+
+class Memory {
+  friend class MMU;
+  public:
+    Memory(Config& cf): nextAvailableFrame(0) {};
+}
